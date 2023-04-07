1b8a4f6ab9df3f32f56bbfad006f1c0dc271c31f
Tyler Randall <tyler7r@gmail.com>
2023-04-07 09:57:07 -0400

    Retrieve character data from firestore

--- src/components/CharSelect.js
+++ src/components/CharSelect.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 export const CharSelect = (props) => {
+    console.log(props.location);
     if (props.showMenu === false) return
     else if (props.showMenu === true) {
         const menuStyle = {
             position: 'absolute',
-            top: `${props.coords[1]}px`,
-            left: `${props.coords[0]}px`,
+            top: `${(props.location[1])}px`,
+            left: `${props.location[0]}px`,
         }
 
         const charClick = () => {
@@ -15,7 +16,7 @@ export const CharSelect = (props) => {
 
         return (
             <div style={menuStyle} className='char-select'>
-                <div className='char'>Waldo</div>
+                <div onClick={() => props.handleMenu(props.coords[0], props.coords[1])} className='char'>Waldo</div>
                 <div className='char'>Wizard</div>
                 <div className='char'>Odlaw</div>
             </div>
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,34 +1,54 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { CharSelect } from './CharSelect';
 import './styles/game.css';
+import { getDoc, doc, onSnapshot, Firestore } from 'firebase/firestore';
+import { db } from '../firebase';
 
 export const Game = (props) => {
     const [coords, setCoords] = useState([0, 0]);
 
     const [showMenu, setShowMenu] = useState(false);
 
+    const [charCoods, setCharCoords] = useState({});
+
+    const [targetLocation, setTargetLocation] = useState([]);
+
     const imgRef = useRef();
 
     const handleClick = (e) => {
         e.preventDefault();
-        const nav = document.querySelector('#main-nav').clientHeight;
-        setCoords([e.pageX, e.pageY - nav]);
 
-        setShowMenu(true);
+        let xCoord = Math.round((e.nativeEvent.offsetX / e.nativeEvent.target.offsetWidth) * 100)
+        let yCoord = Math.round((e.nativeEvent.offsetY / e.nativeEvent.target.offsetHeight) * 100)
+        console.log(xCoord, yCoord);
+
+        setTargetLocation([e.pageX, e.pageY])
+
+        setCoords([xCoord, yCoord]);
+
+        setShowMenu(!showMenu);
+    }
+
+    const handleMenu = async (x, y) => {
+        const answersRef = doc(db, 'answers', `${props.level}`);
+
+        const answers = await getDoc(answersRef)
+
+        setCharCoords(answers.data());
     }
 
     if (props.level === 'easy') {
         return (
             <div ref={imgRef}>
-                <img onClick={handleClick} className='game-img' src={require('./images/ski.jpg')} alt='ski'/>
-                <CharSelect coords={coords} showMenu={showMenu} />
+                <img onClick={(e) => {handleClick(e); handleMenu()}} className='game-img' src={require('./images/ski.jpg')} alt='ski'/>
+                <CharSelect coords={coords} showMenu={showMenu} handleMenu={handleMenu} location={targetLocation} />
             </div>
         )
     } else if (props.level === 'hard') {
         return (
             <div ref={imgRef}>
                 <img onClick={(e) => handleClick(e)} className='game-img' src={require('./images/track.jpg')} alt='track'/>
-                <CharSelect showMenu={showMenu} coords={coords} />
+                <CharSelect showMenu={showMenu} coords={coords} handleMenu={handleMenu} location={targetLocation} />
             </div>
         )
     }
