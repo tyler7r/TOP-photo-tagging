a4866871aff04977df3c45be669162057a354052
Tyler Randall <tyler7r@gmail.com>
2023-04-10 12:42:35 -0400

    Initial game board styling

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -4,20 +4,19 @@ export const CharacterList = (props) => {
     if (props.remainingChars.length > 0) {
     return (
         <div id='character-list'>
-            <div id='character-list-msg'>Remaining Characters: </div>
             {props.remainingChars.map(char => {
                 return (
                     <div key={char} className='character-list-item'>
-                        <div className='character-list-name'>{char}</div>
+                        <div className='character-list-name'>{char.toUpperCase()}</div>
                         <img className='character-list-img' src={require(`./images/${char}.jpg`)} alt={char} />
                     </div>
                 )
             })}
-        </div> 
+        </div>
     )} else if (props.remainingChars.length === 0) {
         return (
         <div id='character-list'>
-            <div id='empty-characater-list-msg'>All Characters Found!</div>
+            <div id='empty-character-list-msg'>YOU WON!</div>
         </div>
         )
     }
