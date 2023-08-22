6e33a227425883a50eef8ad7233f641417cbe139
Tyler Randall <tyler7r@gmail.com>
2023-08-22 12:15:37 -0400

    Prepare for netlify deploy

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,14 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDGkx1m9ULp2gIVyEfHj2ZHVPgnEKN-8WY",
-  authDomain: "top-photo-tagging-439e1.firebaseapp.com",
-  projectId: "top-photo-tagging-439e1",
-  storageBucket: "top-photo-tagging-439e1.appspot.com",
-  messagingSenderId: "770198789967",
-  appId: "1:770198789967:web:bb492b814f690c3f2f6f3a",
-  measurementId: "G-40QF0BX7DK"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
